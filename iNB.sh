7bc561b88d276fb682e0d9150b12c209caddc6a7
gutosie <krzysztofgutosie@gmail.com>
2021-02-09 12:56:45 +0200

    Add files via upload

---
+++ iNB.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+#skrypt instaluje neoboot-a
+#
+if `grep -q 'osd.language=pl_PL' </etc/enigma2/settings`; then
+  PL=1
+fi
+[ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
+[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+[ $PL ] && echo "Pobieranie archiwum..." || echo "Downloading archive file..."
+URL='https://github.com/gutosie/neoboot/archive/main.zip'
+curl -kLs $URL  -o /tmp/neoboot.zip
+cd /tmp/
+if [ ! -e /tmp/master.zip ]; then 
+   wget $URL  
+   mv -f /tmp/master.zip /tmp/neoboot.zip  
+fi
+unzip -qn ./neoboot.zip
+rm -f /tmp/neoboot.zip
+#kopiowanie
+[ $PL ] && echo "Instalowanie..." || echo "Instaling..."
+Cel="/usr/lib/enigma2/python/Plugins/Extensions"
+[ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
+mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
+[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+cd $Cel/NeoBoot
+chmod 755 ./bin/*
+chmod 755 ./ex_init.py
+chmod 755 ./files/*.sh
+chmod -R +x ./ubi_reader/*
+if [ $PL ] ; then
+  echo ""
+  echo "#####################################################"
+  echo "#          NEOBOOT ZOSTAL ZAINSTALOWANY             #"
+  echo "#####################################################"
+  echo "#                ZRESTARTUJ TUNER                   #"
+  echo "#####################################################"
+  echo ""
+else
+  echo ""
+  echo "#####################################################"
+  echo "#          NEOBOOT INSTALLED SUCCESSFULLY           #"
+  echo "#####################################################"
+  echo "#             PLEASE RESTART YOUR STB               #"
+  echo "#####################################################"
+  echo ""
+fi
+exit 0     
