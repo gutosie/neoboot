1782eab0661eb0da30f21f888cc141de961c5dd3
gutosie <krzysztofgutosie@gmail.com>
2021-03-12 14:35:23 +0200

    fix mips stb

--- NeoBoot/tmpfiles/target/vu_dev_mtd1.sh
+++ NeoBoot/tmpfiles/target/vu_dev_mtd1.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-#script - gutosie 
+#script - gutosie
+if `grep -q 'osd.language=pl_PL' </etc/enigma2/settings`; then
+  PL=1
+fi
 
 if [ -f /proc/stb/info/vumodel ];  then  
     VUMODEL=$( cat /proc/stb/info/vumodel )     
@@ -25,10 +28,10 @@ KERNEL=`uname -r`
 IMAGE=ImageBoot
 IMAGENEXTBOOT=/ImageBoot/.neonextboot
 NEOBOOTMOUNT=$( cat /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/.location) 
-BOXNAME=$( cat /etc/hostname) 
-# $NEOBOOTMOUNT$IMAGE 
-# $NEOBOOTMOUNT
+BOXNAME=$( cat /etc/hostname)
 UPLOAD=ImagesUpload
+NandWrite=/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot//bin/nandwrite
+#nandwrite -p /dev/mtd1 /media/hdd/ImagesUpload/.kernel/vusolo.vmlinux.gz
 
 if [ -f $NEOBOOTMOUNT$IMAGENEXTBOOT ]; then
   TARGET=`cat $NEOBOOTMOUNT$IMAGENEXTBOOT`
@@ -37,87 +40,87 @@ else
 fi
                    
 if [ $VUMODEL = "bm750" ] || [ $BOXNAME = "vuduo" ] || [ $BOXNAME = "vusolo" ] || [ $BOXNAME = "vuuno" ] || [ $BOXNAME = "vuultimo" ]; then
-    if [ $TARGET = "Flash" ]; then                    
+    if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ] ; then
+        if [ $TARGET = "Flash" ]; then                    
                 if [ -e /.multinfo ]; then                                                
                             if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ]; then
                                 if [ -e $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz ] ; then
-                                    echo "Kasowanie kernel z /dev/mtd1..."
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd1..." || echo "Erase kernel from  /dev/mtd1"
+                                    
                                     sleep 2                                
-                                    flash_eraseall /dev/mtd1  
-                                    echo "Instalacja kernel do /dev/mtd1..." 
+                                    flash_eraseall /dev/mtd1 > /dev/null 2>&1
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd1..." || echo "Writing kernel to  /dev/mtd1"  
                                     sleep 2
-		                                nandwrite -p /dev/mtd1 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz
+		                                $NandWrite -p /dev/mtd1 /media/hdd/.kernel/vusolo.vmlinux.gz > /dev/null 2>&1
                                 fi
                             fi
                             update-alternatives --remove vmlinux vmlinux-`uname -r` || true                                          
-                            echo "NEOBOOT is booting image from " $TARGET
+                            echo "NEOBOOT is booting image " $TARGET
                             echo "Used Kernel: " $TARGET > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel                           
 
                 elif [ ! -e /.multinfo ]; then
                             if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ]; then 
                                 if [ $VUMODEL = "bm750" ] || [ $VUMODEL = "duo" ] || [ $VUMODEL = "solo" ] || [ $VUMODEL = "uno" ] || [ $VUMODEL = "ultimo" ]; then                    
                                     if [ -e $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz ] ; then
-                                        echo "Kasowanie kernel z /dev/mtd1..."
+                                        [ $PL ] && echo "Kasowanie kernel z /dev/mtd1..." || echo "Erase kernel from  /dev/mtd1"                                   
                                         sleep 2
-                                        flash_eraseall /dev/mtd1   
-                                        echo "Wgrywanie kernel do /dev/mtd1..."
+                                        flash_eraseall /dev/mtd1 > /dev/null 2>&1
+                                        [ $PL ] && echo "Wgrywanie kernel do /dev/mtd1..." || echo "Writing kernel to  /dev/mtd1"                                           
                                         sleep 2
-                                        nandwrite -p /dev/mtd1 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz
+                                        $NandWrite -p /dev/mtd1 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz > /dev/null 2>&1
                                     fi
                                 fi
                             fi    
                             update-alternatives --remove vmlinux vmlinux-`uname -r` || true
+                            echo "NEOBOOT is booting image " $TARGET
                             echo "Used Kernel: " $TARGET > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel
-                            echo " NEOBOOT - zainstalowano kernel-image - " $TARGET  "Za chwile nastapi restart systemu !!!" 
                 fi
                 echo "...............Shutdown Now..............."
                 sync && echo 3 > /proc/sys/vm/drop_caches
-                sleep 10 
+                sleep 10
+                PATH=/sbin:/bin:/usr/sbin:/usr/bin 
                 reboot -d -f 
-    else              	    
-        if [ $TARGET != "Flash" ]; then 
-            if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ] ; then	     
-                if  [ $VUMODEL = "bm750" ] || [ $VUMODEL = "duo" ] || [ $VUMODEL = "solo" ] || [ $VUMODEL = "uno" ] || [ $VUMODEL = "ultimo" ]; then
+        else                  	    
                         if [ -e /.multinfo ] ; then
                                 INFOBOOT=$( cat /.multinfo )
                                 if [ $TARGET = $INFOBOOT ] ; then
-                                    echo "NEOBOOT is booting image from " $TARGET                                    
+                                    echo "NEOBOOT is booting image " $TARGET                                    
                                 else                                    
-                                    echo "Kasowanie kernel z /dev/mtd1"
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd1..." || echo "Erase kernel from  /dev/mtd1"                                   
                                     sleep 2
-                                    flash_eraseall /dev/mtd1
-                                    echo "Wgrywanie kernel do /dev/mtd1"                                    
+                                    flash_eraseall /dev/mtd1 > /dev/null 2>&1
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd1..." || echo "Writing kernel to  /dev/mtd1"                                                                      
                                     sleep 2
-		                    nandwrite -p /dev/mtd1 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz  
+                                    $NandWrite -p /dev/mtd1 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz > /dev/null 2>&1  
                                     update-alternatives --remove vmlinux vmlinux-`uname -r` || true
-                                    echo "Kernel dla potrzeb startu systemu " $TARGET " z procesorem mips zostal zmieniony!!!"
+                                    echo "NEOBOOT is booting image" $TARGET
                                     echo "Used Kernel: " $TARGET   > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel
-                                    echo " NEOBOOT - zainstalowano kernel-image - " $TARGET  "Za chwile nastapi restart systemu !!!" 
                                 fi
                         else
-                                    echo "Kasowanie kernel z /dev/mtd1"
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd1..." || echo "Erase kernel from  /dev/mtd1"                                   
                                     sleep 2
-                                    flash_eraseall /dev/mtd1 
-                                    echo "Wgrywanie kernel do /dev/mtd1"
-                                    sleep 2                                                     
-		                    nandwrite -p /dev/mtd1 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz                                                                                                     
+                                    flash_eraseall /dev/mtd1 > /dev/null 2>&1 
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd1..." || echo "Writing kernel to  /dev/mtd1"
+                                    sleep 2
+                                    $NandWrite -p /dev/mtd1 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz > /dev/null 2>&1                                                                                                     
                                     update-alternatives --remove vmlinux vmlinux-`uname -r` || true
-                                    echo "Kernel dla potrzeb startu systemu " $TARGET " z procesorem mips zostal zmieniony!!!"
+                                    echo "NEOBOOT is booting image " $TARGET
                                     echo "Used Kernel: " $TARGET   > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel                                       
                         fi                
                         echo "...............Shutdown Now..............." 
                         sync && echo 3 > /proc/sys/vm/drop_caches
-                        sleep 5 
-                        reboot -d -f 
-                fi
-            fi
+                        sleep 5
+                        PATH=/sbin:/bin:/usr/sbin:/usr/bin 
+                        reboot -d -f
         fi
-    fi                               
+    else
+        break;
+    fi
 else
                     ln -sfn /sbin/init.sysvinit /sbin/init
                     echo "CHIPSET: " $CHIPSET " BOXNAME: "$BOXNAME" MODEL: "$VUMODEL" "
                     echo "$TARGET "  > $NEOBOOTMOUNT/ImageBoot/.neonextboot
-                    echo "Error - Nie wpierany model STB !!! "
+                    [ $PL ] && echo "Ten model stb nie jest wpierany" || echo "This stb model is not supported." 
                     exit 0
 fi
 exit 0
--- NeoBoot/tmpfiles/target/vu_dev_mtd2.sh
+++ NeoBoot/tmpfiles/target/vu_dev_mtd2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-#script - gutosie 
+#script - gutosie
+if `grep -q 'osd.language=pl_PL' </etc/enigma2/settings`; then
+  PL=1
+fi
 
 if [ -f /proc/stb/info/vumodel ];  then  
     VUMODEL=$( cat /proc/stb/info/vumodel )     
@@ -25,10 +28,10 @@ KERNEL=`uname -r`
 IMAGE=ImageBoot
 IMAGENEXTBOOT=/ImageBoot/.neonextboot
 NEOBOOTMOUNT=$( cat /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/.location) 
-BOXNAME=$( cat /etc/hostname) 
-# $NEOBOOTMOUNT$IMAGE 
-# $NEOBOOTMOUNT
+BOXNAME=$( cat /etc/hostname)
 UPLOAD=ImagesUpload
+NandWrite=/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot//bin/nandwrite
+#nandwrite -p /dev/mtd2 /media/hdd/ImagesUpload/.kernel/vusolo.vmlinux.gz
 
 if [ -f $NEOBOOTMOUNT$IMAGENEXTBOOT ]; then
   TARGET=`cat $NEOBOOTMOUNT$IMAGENEXTBOOT`
@@ -37,81 +40,87 @@ else
 fi
                    
 if  [ $BOXNAME = "vusolo2" ] || [ $BOXNAME = "vuduo2" ] || [ $BOXNAME = "vusolose" ] || [ $BOXNAME = "vuzero" ]; then     #[ $BOXNAME = "mbultra" ]
-    if [ $TARGET = "Flash" ]; then                    
+    if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ] ; then
+        if [ $TARGET = "Flash" ]; then                    
                 if [ -e /.multinfo ]; then                                                
                             if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ]; then
                                 if [ -e $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz ] ; then
-                                    echo "Kasowanie kernel z /dev/mtd2..."
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd2..." || echo "Erase kernel from  /dev/mtd2"
+                                    
                                     sleep 2                                
-                                    flash_eraseall /dev/mtd2  
-                                    echo "Instalacja kernel do /dev/mtd2..." 
+                                    flash_eraseall /dev/mtd2 > /dev/null 2>&1
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd2..." || echo "Writing kernel to  /dev/mtd2"  
                                     sleep 2
-		                                nandwrite -p /dev/mtd2 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz
+		                                $NandWrite -p /dev/mtd2 /media/hdd/.kernel/vusolo.vmlinux.gz > /dev/null 2>&1
                                 fi
                             fi
                             update-alternatives --remove vmlinux vmlinux-`uname -r` || true                                          
-                            echo "NEOBOOT is booting image from " $TARGET
+                            echo "NEOBOOT is booting image " $TARGET
                             echo "Used Kernel: " $TARGET > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel                           
 
                 elif [ ! -e /.multinfo ]; then
-                            if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ]; then                    
+                            if [ -f /proc/stb/info/vumodel ] || [ ! -e /proc/stb/info/boxtype ]; then 
+                                if [ $VUMODEL = "bm750" ] || [ $VUMODEL = "duo" ] || [ $VUMODEL = "solo" ] || [ $VUMODEL = "uno" ] || [ $VUMODEL = "ultimo" ]; then                    
                                     if [ -e $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz ] ; then
-                                        echo "Kasowanie kernel z /dev/mtd2..."
+                                        [ $PL ] && echo "Kasowanie kernel z /dev/mtd2..." || echo "Erase kernel from  /dev/mtd2"                                   
                                         sleep 2
-                                        flash_eraseall /dev/mtd2   
-                                        echo "Wgrywanie kernel do /dev/mtd2..."
+                                        flash_eraseall /dev/mtd2 > /dev/null 2>&1
+                                        [ $PL ] && echo "Wgrywanie kernel do /dev/mtd2..." || echo "Writing kernel to  /dev/mtd2"                                           
                                         sleep 2
-                                        nandwrite -p /dev/mtd2 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz
+                                        $NandWrite -p /dev/mtd2 $NEOBOOTMOUNT$UPLOAD/.kernel/$BOXNAME.vmlinux.gz > /dev/null 2>&1
                                     fi
+                                fi
                             fi    
                             update-alternatives --remove vmlinux vmlinux-`uname -r` || true
+                            echo "NEOBOOT is booting image " $TARGET
                             echo "Used Kernel: " $TARGET > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel
-                            echo " NEOBOOT - zainstalowano kernel-image - " $TARGET  "Za chwile nastapi restart systemu !!!" 
                 fi
                 echo "...............Shutdown Now..............."
                 sync && echo 3 > /proc/sys/vm/drop_caches
-                sleep 10 
+                sleep 10
+                PATH=/sbin:/bin:/usr/sbin:/usr/bin 
                 reboot -d -f 
-    else              	    
-        if [ $TARGET != "Flash" ]; then 	     
+        else              	    
                         if [ -e /.multinfo ] ; then
                                 INFOBOOT=$( cat /.multinfo )
                                 if [ $TARGET = $INFOBOOT ] ; then
-                                    echo "NEOBOOT is booting image from " $TARGET                                    
+                                    echo "NEOBOOT is booting image " $TARGET                                    
                                 else                                    
-                                    echo "Kasowanie kernel z /dev/mtd2"
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd2..." || echo "Erase kernel from  /dev/mtd2"                                   
                                     sleep 2
-                                    flash_eraseall /dev/mtd2
-                                    echo "Wgrywanie kernel do /dev/mtd2"                                    
+                                    flash_eraseall /dev/mtd2 > /dev/null 2>&1
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd2..." || echo "Writing kernel to  /dev/mtd2"                                                                      
                                     sleep 2
-		                    nandwrite -p /dev/mtd2 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz  
+                                    $NandWrite -p /dev/mtd2 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz > /dev/null 2>&1  
                                     update-alternatives --remove vmlinux vmlinux-`uname -r` || true
-                                    echo "Kernel dla potrzeb startu systemu " $TARGET " z procesorem mips zostal zmieniony!!!"
+                                    echo "NEOBOOT is booting image" $TARGET
                                     echo "Used Kernel: " $TARGET   > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel
-                                    echo " NEOBOOT - zainstalowano kernel-image - " $TARGET  "Za chwile nastapi restart systemu !!!" 
                                 fi
                         else
-                                    echo "Kasowanie kernel z /dev/mtd2"
+                                    [ $PL ] && echo "Kasowanie kernel z /dev/mtd2..." || echo "Erase kernel from  /dev/mtd2"                                   
                                     sleep 2
-                                    flash_eraseall /dev/mtd2 
-                                    echo "Wgrywanie kernel do /dev/mtd2"
-                                    sleep 2                                                     
-		                    nandwrite -p /dev/mtd2 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz                                                                                                     
+                                    flash_eraseall /dev/mtd2 > /dev/null 2>&1 
+                                    [ $PL ] && echo "Wgrywanie kernel do /dev/mtd2..." || echo "Writing kernel to  /dev/mtd2"
+                                    sleep 2
+                                    $NandWrite -p /dev/mtd2 $NEOBOOTMOUNT$IMAGE/$TARGET/boot/$BOXNAME.vmlinux.gz > /dev/null 2>&1                                                                                                     
                                     update-alternatives --remove vmlinux vmlinux-`uname -r` || true
-                                    echo "Kernel dla potrzeb startu systemu " $TARGET " z procesorem mips zostal zmieniony!!!"
+                                    echo "NEOBOOT is booting image " $TARGET
                                     echo "Used Kernel: " $TARGET   > $NEOBOOTMOUNT$UPLOAD/.kernel/used_flash_kernel                                       
                         fi                
                         echo "...............Shutdown Now..............." 
                         sync && echo 3 > /proc/sys/vm/drop_caches
-                        sleep 5 
+                        sleep 5
+                        PATH=/sbin:/bin:/usr/sbin:/usr/bin 
                         reboot -d -f 
-        fi
-    fi                               
+        fi                               
+    else
+        break;
+    fi
 else
                     ln -sfn /sbin/init.sysvinit /sbin/init
                     echo "CHIPSET: " $CHIPSET " BOXNAME: "$BOXNAME" MODEL: "$VUMODEL" "
                     echo "$TARGET "  > $NEOBOOTMOUNT/ImageBoot/.neonextboot
-                    echo "Error - Nie wpierany model STB !!! "
+                    [ $PL ] && echo "Ten model stb nie jest wpierany" || echo "This stb model is not supported." 
                     exit 0
 fi
 exit 0
