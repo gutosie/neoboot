b668b0c2b9b67073734cfa4c57cb9eb7e02928bf
gutosie <krzysztofgutosie@gmail.com>
2021-03-19 11:57:35 +0200

    Update findsk.sh

--- NeoBoot/files/findsk.sh
+++ NeoBoot/files/findsk.sh
@@ -6,12 +6,6 @@ do
      if [ "$key" == "root" ]; then
           ln -sf "$value" /media/root
      fi
-done
-sleep 2
-for line in ${args};
-do
-     key=${line%%=*}
-     value=${line#*=}
      if [ "$key" == "kernel" ]; then
           ln -sf "$value" /dev/kernel
      fi
