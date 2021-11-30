f65c997ba5a7e84ee1557baa82cd6fffe2ac1d99
gutosie <krzysztofgutosie@gmail.com>
2021-11-30 15:33:47 +0200

    Update plugin.py

--- NeoBoot/plugin.py
+++ NeoBoot/plugin.py
@@ -57,7 +57,7 @@ else:
 loggscrash = time.localtime(time.time())
 PLUGINVERSION = '9.32'
 UPDATEVERSION = '9.32'
-UPDATEDATE = '"+%Y12%d"'
+UPDATEDATE = '"+%Y09%d"'
 LinkNeoBoot = '/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot'
 
 try:
