5b57328753bf1dedddbed2e38945e4b140b23afb
gutosie <krzysztofgutosie@gmail.com>
2022-01-21 11:07:41 +0200

    Update ex_init.py

--- NeoBoot/ex_init.py
+++ NeoBoot/ex_init.py
@@ -2,7 +2,7 @@
 
 import sys
 import extract
-if len(sys.argv) < 15:
+if len(sys.argv) < 17:
     pass
 else:
-    extract.NEOBootMainEx(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12], sys.argv[13], sys.argv[14], sys.argv[15], sys.argv[16])
+    extract.NEOBootMainEx(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12], sys.argv[13], sys.argv[14], sys.argv[15], sys.argv[16], sys.argv[17] )
