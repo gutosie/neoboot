7afd80c95289bbd6f0b9e4a19d5294508de14497
gutosie <krzysztofgutosie@gmail.com>
2022-03-03 15:12:37 +0200

    Update iNB.sh

--- iNB.sh
+++ iNB.sh
@@ -13,82 +13,94 @@ elif [ -f /etc/opkg/opkg.conf ] ; then
    STATUS='/var/lib/opkg/status'
    OS='Opensource'
 fi
-[ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
-[ -e /tmp/neoboot-main ] && rm -rf /tmp/neoboot-main
-[ $PL ] && echo "Pobieranie archiwum..." || echo "Downloading archive file..."
-URL='https://github.com/gutosie/neoboot/archive/main.zip'
-curl -kLs $URL  -o /tmp/neoboot.zip
-Cel="/usr/lib/enigma2/python/Plugins/Extensions"
-
-if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
-    break;
-else
-    if [ -e $Cel/NeoBoot/plugin.py ]; then 
-       chattr -i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.py; chattr -i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.pyo
+if [ ! -e /.multinfo ]; then
+    [ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
+    [ -e /tmp/neoboot-main ] && rm -rf /tmp/neoboot-main
+    [ $PL ] && echo "Pobieranie archiwum..." || echo "Downloading archive file..."
+    URL='https://github.com/gutosie/neoboot/archive/main.zip'
+    curl -kLs $URL  -o /tmp/neoboot.zip
+    Cel="/usr/lib/enigma2/python/Plugins/Extensions"
+    if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
+        break;
+    else
+        if [ -e $Cel/NeoBoot/plugin.py ]; then 
+           chattr -i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.py; chattr -i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.pyo
+        fi
+        if [ -e /usr/lib/periodon/.activatedmac ]; then 
+           chattr -i /usr/lib/periodon/.activatedmac; rm -f /usr/lib/periodon/.activatedmac
+        fi
     fi
-    if [ -e /usr/lib/periodon/.activatedmac ]; then 
-       chattr -i /usr/lib/periodon/.activatedmac; rm -f /usr/lib/periodon/.activatedmac
+    cd /tmp/
+    #pobieranie
+    if [ ! -e /tmp/neoboot.zip ]; then 
+       wget --no-check-certificate $URL  
+       mv -f /tmp/main.zip /tmp/neoboot.zip  
     fi
-fi
-cd /tmp/
-#pobieranie
-if [ ! -e /tmp/neoboot.zip ]; then 
-   wget --no-check-certificate $URL  
-   mv -f /tmp/main.zip /tmp/neoboot.zip  
-fi
-if [ ! -e /tmp/neoboot.zip ]; then 
-   fullwget --no-check-certificate $URL  
-   mv -f /tmp/main.zip /tmp/neoboot.zip  
-fi
-unzip -qn ./neoboot.zip
-rm -f /tmp/neoboot.zip
-[ -e /tmp/main.zip ] && rm -rf /tmp/main.zip
-
-if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
-    cp -af /tmp/neoboot-main/NeoBoot/tmpfiles/runpy/mips_run.py /tmp/neoboot-main/NeoBoot/run.py; rm -r /tmp/neoboot-main/NeoBoot/ubi_reader*;  rm -r /tmp/neoboot-main/NeoBoot/tmpfiles; rm -r /tmp/neoboot-main/NeoBoot/bin/neoinitar*;  rm -r /tmp/neoboot-main/NeoBoot/bin/nanddump*; rm -r /tmp/neoboot-main/NeoBoot/bin/fbcle*; rm -r /tmp/neoboot-main/NeoBoot/bin/neob*; rm -r /tmp/neoboot-main/NeoBoot/bin/nandwrite; rm -r /tmp/neoboot-main/NeoBoot/bin/neoinitmips_vu
-fi
-
-#kopiowanie
-if [ -e $Cel/NeoBoot/.location ]; then 
-   rm -rf $Cel/NeoBoot/.location   
-fi
-[ $PL ] && echo "Instalowanie..." || echo "Instaling..."
-[ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
-mv -f /tmp/neoboot-main/NeoBoot/files/testinout /usr/lib/enigma2/python/Tools/Testinout.py
-mkdir -p /usr/lib/periodon
-mv -f /tmp/neoboot-main/NeoBoot/* $Cel/NeoBoot
-[ -e /tmp/neoboot-main ] && rm -rf /tmp/neoboot-main
-cd $Cel/NeoBoot
-chmod 755 ./bin/*
-chmod 755 ./ex_init.py
-chmod 755 ./files/*.sh
-if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
-    break;
+    if [ ! -e /tmp/neoboot.zip ]; then 
+       fullwget --no-check-certificate $URL  
+       mv -f /tmp/main.zip /tmp/neoboot.zip  
+    fi
+    unzip -qn ./neoboot.zip
+    rm -f /tmp/neoboot.zip
+    [ -e /tmp/main.zip ] && rm -rf /tmp/main.zip
+    if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
+        cp -af /tmp/neoboot-main/NeoBoot/tmpfiles/runpy/mips_run.py /tmp/neoboot-main/NeoBoot/run.py; rm -r /tmp/neoboot-main/NeoBoot/ubi_reader*;  rm -r /tmp/neoboot-main/NeoBoot/tmpfiles; rm -r /tmp/neoboot-main/NeoBoot/bin/neoinitar*;  rm -r /tmp/neoboot-main/NeoBoot/bin/nanddump*; rm -r /tmp/neoboot-main/NeoBoot/bin/fbcle*; rm -r /tmp/neoboot-main/NeoBoot/bin/neob*; rm -r /tmp/neoboot-main/NeoBoot/bin/nandwrite; rm -r /tmp/neoboot-main/NeoBoot/bin/neoinitmips_vu
+    fi
+    #kopiowanie
+    if [ -e $Cel/NeoBoot/.location ]; then 
+       rm -rf $Cel/NeoBoot/.location   
+    fi
+    [ $PL ] && echo "Instalowanie..." || echo "Instaling..."
+    [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
+    mv -f /tmp/neoboot-main/NeoBoot/files/testinout /usr/lib/enigma2/python/Tools/Testinout.py
+    mkdir -p /usr/lib/periodon
+    mv -f /tmp/neoboot-main/NeoBoot/* $Cel/NeoBoot
+    [ -e /tmp/neoboot-main ] && rm -rf /tmp/neoboot-main
+    cd $Cel/NeoBoot
+    chmod 755 ./bin/*
+    chmod 755 ./ex_init.py
+    chmod 755 ./files/*.sh
+    if [ $BOXHOSTNAME = "dm500hd" ] || [ $BOXHOSTNAME = "dm800se" ] || [ $BOXHOSTNAME = "dm800" ] || [ $BOXHOSTNAME = "dm8000" ]; then
+        break;
+    else
+        chmod -R +x ./ubi_reader_arm/*
+        chmod -R +x ./ubi_reader_mips/*
+        chattr +i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.py    
+    fi
+    if [ $PL ] ; then
+      echo ""
+      echo "#####################################################"
+      echo "#          NEOBOOT ZOSTAL ZAINSTALOWANY             #"
+      echo "#####################################################"
+      echo ""
+    else
+      echo ""
+      echo "#####################################################"
+      echo "#     >>> NEOBOOT INSTALLED SUCCESSFULLY <<<        #"
+      echo "#####################################################"
+      echo ""
+    fi
+    echo "          ----- Restart Enigma2 GUI ... -----               "
+    sleep 2
+    if [ $OS = 'DreamOS' ]; then 
+        systemctl restart enigma2
+    else
+        killall -9 enigma2
+    fi
 else
-    chmod -R +x ./ubi_reader_arm/*
-    chmod -R +x ./ubi_reader_mips/*
-    chattr +i /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/plugin.py    
-fi
-
-if [ $PL ] ; then
-  echo ""
-  echo "#####################################################"
-  echo "#          NEOBOOT ZOSTAL ZAINSTALOWANY             #"
-  echo "#####################################################"
-  echo ""
-else
-  echo ""
-  echo "#####################################################"
-  echo "#     >>> NEOBOOT INSTALLED SUCCESSFULLY <<<        #"
-  echo "#####################################################"
-  echo ""
-fi
-echo "          ----- Restart Enigma2 GUI ... -----               "
-sleep 2
-if [ $OS = 'DreamOS' ]; then 
-    systemctl restart enigma2
-else
-    killall -9 enigma2
+    if [ $PL ] ; then
+      echo ""
+      echo "#####################################################"
+      echo ">>>>       Error! To nie jest image flash.       <<<<"
+      echo "#####################################################"
+      echo ""
+    else
+      echo ""
+      echo "#####################################################"
+      echo ">>>>      Error!  Go back to image flash.       <<<<#"
+      echo "#####################################################"
+      echo ""
+    fi
 fi
 exit 0 
 
