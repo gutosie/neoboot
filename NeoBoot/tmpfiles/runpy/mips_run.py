aa349a9a01365fbbf91034dd9dd0ac5d2731ddd7
gutosie <krzysztofgutosie@gmail.com>
2022-03-05 20:18:04 +0200

    Add files via upload

--- NeoBoot/tmpfiles/runpy/mips_run.py
+++ NeoBoot/tmpfiles/runpy/mips_run.py
@@ -135,7 +135,7 @@ class StartImage(Screen):
                             
 def getTurnOffOnSystem():               
         for line in open("/etc/hostname"):                                
-                        if "dm500hd" in line or "dm800se" in line or "dm800" in line or "dm8000" in line:
+                        if "dm500hd" in line or "dm800se" in line or "dm800" in line or "dm800se" in line or "dm8000" in line:
                                 if fileExists('%sImageBoot/%s/squashfs-images' % (getNeoLocation(), getImageNeoBoot())):
                                     os.system('ln -sf "%sImageBoot/%s/squashfs-images" "//squashfs-images"' % (getNeoLocation(), getImageNeoBoot()))
                         os.system('echo 3 > /proc/sys/vm/drop_caches; shutdown -r now & sleep 2 & reboot -d -f &')
