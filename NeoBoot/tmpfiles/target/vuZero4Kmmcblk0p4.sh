cf35faa4a40441b3c1dbe4fb0180d36bad1199d6
gutosie <krzysztofgutosie@gmail.com>
2022-03-05 19:44:54 +0200

    Update vuZero4Kmmcblk0p4.sh

--- NeoBoot/tmpfiles/target/vuZero4Kmmcblk0p4.sh
+++ NeoBoot/tmpfiles/target/vuZero4Kmmcblk0p4.sh
@@ -74,7 +74,7 @@ if [ $VUMODEL = "zero4k" ]   ; then
                 echo "...............shutdown now..............." 
                 sleep 5 
                 echo -n "Rebooting... "
-                reboot
+                /sbin/reboot
     else              	    
         if [ $TARGET != "Flash" ]; then                                             
                         if [ -e /.multinfo ] ; then
@@ -126,7 +126,7 @@ if [ $VUMODEL = "zero4k" ]   ; then
                         echo "...............Shutdown Now..............."
                         sleep 5
                         PATH=/sbin:/bin:/usr/sbin:/usr/bin
-                        reboot 
+                        /sbin/reboot 
         fi
     fi                               
 else
