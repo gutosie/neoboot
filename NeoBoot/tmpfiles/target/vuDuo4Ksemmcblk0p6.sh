5f4a80817a16be478b1b2dbbc532c96b6043fc8d
gutosie <krzysztofgutosie@gmail.com>
2022-03-06 09:02:57 +0200

    Update vuDuo4Ksemmcblk0p6.sh

--- NeoBoot/tmpfiles/target/vuDuo4Ksemmcblk0p6.sh
+++ NeoBoot/tmpfiles/target/vuDuo4Ksemmcblk0p6.sh
@@ -70,7 +70,7 @@ if [ $VUMODEL = "duo4kse" ] ; then
                 sync && echo 3 > /proc/sys/vm/drop_caches
                 sleep 5
                 echo -n "Rebooting... "
-                reboot -d -i; shutdown now -r
+                /sbin/init.sysvinit 6 #reboot -d -i; shutdown now -r
     else              	    
         if [ $TARGET != "Flash" ]; then                    
                         [ $PL ] && echo "Przenoszenie pliku kernel do /tmp..." || echo "Moving the kernel file to..."
