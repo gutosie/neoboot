622b1b12567cc555fef9bf82b7b2d6adb140c9e5
gutosie <krzysztofgutosie@gmail.com>
2022-04-05 19:35:43 +0300

    fix duo4kse problem reboot

--- NeoBoot/plugin.py
+++ NeoBoot/plugin.py
@@ -53,8 +53,8 @@ if not fileExists('/etc/vtiversion.info') and not fileExists('/etc/bhversion') a
 else:
     from Screens.Console import Console
 loggscrash = time.localtime(time.time())
-PLUGINVERSION = '9.53'
-UPDATEVERSION = '9.53'
+PLUGINVERSION = '9.54'
+UPDATEVERSION = '9.54'
 UPDATEDATE = '"+%Y11%d"'
 LinkNeoBoot = '/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot'
 
