7fe9b9cf53ff7312c809c20ee3f2fd85573ebf82
gutosie <krzysztofgutosie@gmail.com>
2022-06-09 15:58:36 +0300

    fix back to flash - vuzero

--- NeoBoot/plugin.py
+++ NeoBoot/plugin.py
@@ -53,8 +53,8 @@ if not fileExists('/etc/vtiversion.info') and not fileExists('/etc/bhversion') a
 else:
     from Screens.Console import Console
 loggscrash = time.localtime(time.time())
-PLUGINVERSION = '9.56'
-UPDATEVERSION = '9.56'
+PLUGINVERSION = '9.57'
+UPDATEVERSION = '9.57'
 UPDATEDATE = '"+%Y05%d"'
 #UPDATEDATE = '"+%Y11%d"'
 LinkNeoBoot = '/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot'
