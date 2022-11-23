deb68b2a266ac02bb55faa3833228d089cabffc3
gutosie <krzysztofgutosie@gmail.com>
2022-11-23 14:02:08 +0200

    add Zgemma H11, Zgemma H5

--- NeoBoot/plugin.py
+++ NeoBoot/plugin.py
@@ -53,8 +53,8 @@ if not fileExists('/etc/vtiversion.info') and not fileExists('/etc/bhversion') a
 else:
     from Screens.Console import Console
 loggscrash = time.localtime(time.time())
-PLUGINVERSION = '9.59'
-UPDATEVERSION = '9.59'
+PLUGINVERSION = '9.60'
+UPDATEVERSION = '9.60'
 UPDATEDATE = '"+%Y11%d"'
 LinkNeoBoot = '/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot'
 
