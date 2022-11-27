8826740596b33497136a6ca6a41264aabe4a9ca5
gutosie <krzysztofgutosie@gmail.com>
2022-11-27 19:36:43 +0200

    Update tools.py

--- NeoBoot/files/tools.py
+++ NeoBoot/files/tools.py
@@ -155,7 +155,9 @@ class MBTools(Screen):
     def updateList(self):
         self.list = []
         mypath = '' + LinkNeoBoot + ''
-        if not fileExists('/tmp/.testneo') and getCheckActivateVip() == getBoxMacAddres() and fileExists('/usr/lib/periodon/.kodn'):        
+        if not fileExists('/tmp/.testneo') and getCheckActivateVip() == getBoxMacAddres() and fileExists('/usr/lib/periodon/.kodn') and not fileExists('/etc/.nameneo '):      
+            os.system(("chattr -i /usr/lib/periodon/.activatedmac; mv /tmp/.mymac /usr/lib/periodon/.activatedmac; chattr +i /usr/lib/periodon/.activatedmac; touch /tmp/.testneo "))
+        elif not fileExists('/tmp/.testneo') and fileExists('/usr/lib/periodon/.kodn') and fileExists('/etc/.nameneo '):
             os.system(("chattr -i /usr/lib/periodon/.activatedmac; mv /tmp/.mymac /usr/lib/periodon/.activatedmac; chattr +i /usr/lib/periodon/.activatedmac; touch /tmp/.testneo "))
         if not fileExists(mypath + 'icons'):
             mypixmap = '' + LinkNeoBoot + '/images/ok.png'
