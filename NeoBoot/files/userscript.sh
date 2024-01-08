ba9c047064572c70e0cb641a9b9b9e6da3c4f98e
gutosie <krzysztofgutosie@gmail.com>
2024-01-08 12:05:56 +0200

    Update userscript.sh

--- NeoBoot/files/userscript.sh
+++ NeoBoot/files/userscript.sh
@@ -18,7 +18,7 @@ fi
 if [ -f /.control_boot_new_image ] ; then
     break ;
 else
-    if [ -f /usr/lib/python2.7 ] ; then
+    #if [ -f /usr/lib/python2.7 ] ; then
         echo "_(Checking internet connection)..."
         ping -c 1 github.com 1>/dev/null 2>%1
         if [ $? -gt 0 ]; then
@@ -26,21 +26,23 @@ else
 		        echo -n "The network has no connection. It is not OK"
 		        echo -n "Network restart..."
 				echo "_____restart network connection_____"
-				/etc/init.d/networking restart;
+				/etc/wpa_supplicant/ifupdown.sh
+                                /etc/init.d/vuplus-wifi-init.sh
+                                /etc/wpa_supplicant/action_wpa.sh
+                                /etc/wpa_supplicant/functions.sh
+				ifconfig sys0 up
+                                /etc/udhcpc.d/50default restart
+                                /etc/init.d/networking restart
 				sleep 5
 		else
 		        echo "github server available"
 		        echo "The network has a connection. It is OK"		        
 		fi
-    else
-        break ;
+    #else
+        #break ;
     
-    fi
+    #fi
 fi
-/etc/init.d/vuplus-wifi-init.sh
-/etc/wpa_supplicant/action_wpa.sh
-/etc/wpa_supplicant/functions.sh
-/etc/wpa_supplicant/ifupdown.sh
 if [ -f /STARTUP ] ; then
     rm -r /STARTU*
 fi
