4a791639afb82f6cf9c48676adaa5cb6b47a6861
gutosie <krzysztofgutosie@gmail.com>
2025-12-11 21:00:42 +0200

    Update extract.py

--- NeoBoot/extract.py
+++ NeoBoot/extract.py
@@ -503,10 +503,10 @@ def NEOBootMainEx(source, target, CopyFiles, CopyKernel, TvList, LanWlan, Sterow
                         cmd1 = 'cp -af /var/spool/cron/* ' + getNeoLocation() + 'ImageBoot/%s/var/spool/cron' % target
                         rc = os.system(cmd1)
                     elif os.path.exists(''+media+'/ImageBoot/'+target+'/etc/cron') and not os.path.exists(''+media+'/ImageBoot/'+target+'/var/spool/cron/crontabs'):
-                        try:
+                        if os.path.exists('/etc/cron'):
                                 cmd1 = 'cp -af /etc/cron/* ' + getNeoLocation() + 'ImageBoot/%s/etc/cron' % target
                                 rc = os.system(cmd1)
-                        except:
+                        elif os.path.exists('/var/spool/cron'):
                                 cmd1 = 'cp -af /var/spool/cron/* ' + getNeoLocation() + 'ImageBoot/%s/etc/cron' % target
                                 rc = os.system(cmd1)
                     os.system('echo "Copied cron files."')
