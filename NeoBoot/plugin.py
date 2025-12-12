809f28580206ab3abe88cb932310b4928151e869
gutosie <krzysztofgutosie@gmail.com>
2025-12-12 15:13:20 +0200

    Update plugin.py

--- NeoBoot/plugin.py
+++ NeoBoot/plugin.py
@@ -53,8 +53,8 @@ if not fileExists('/etc/vtiversion.info') and not fileExists('/etc/bhversion') a
 else:
     from Screens.Console import Console
 loggscrash = time.localtime(time.time())
-PLUGINVERSION = '9.90'
-UPDATEVERSION = '9.90'
+PLUGINVERSION = '9.91'
+UPDATEVERSION = '9.91'
 UPDATEDATE = '"+%Y13%d"'
 LinkNeoBoot = '/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot'
 
